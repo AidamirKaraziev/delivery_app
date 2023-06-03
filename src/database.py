138866d2b7dc197994a4d15561c630df144954f1
azkaraz <karaziev988@gmail.com>
2023-06-03 13:14:22 +0700

    - async changes

--- src/database.py
+++ src/database.py
@@ -1,20 +1,19 @@
+from typing import AsyncGenerator
+
 from sqlalchemy import MetaData
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(engine)
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def get_session():
-    session = Session()
-    try:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
         yield session
-    finally:
-        session.close()
