a6423065415a99a219384e23c122cdd035b97a4e
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-06-06 17:05:34 +0300

    add check current_user

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
 from redis import asyncio as aioredis
 from auth.base_config import auth_backend, fastapi_users
+from auth.models import User
 from auth.schemas import UserRead, UserCreate
 
 from role.router import router as router_role
 from promo.router import router as router_promo
+
+current_user = fastapi_users.current_user()
 
 app = FastAPI(
     title="Delivery App"
@@ -34,3 +37,7 @@ async def startup_event():
     redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
--- src/role/router.py
+++ src/role/router.py
@@ -2,12 +2,16 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from auth.base_config import fastapi_users
+from auth.models import User
 from role.crud import crud_role
 from role.getters import getting_role
 
 from core.response import SingleEntityResponse, ListOfEntityResponse, Meta
 from database import get_async_session
 
+current_active_user = fastapi_users.current_user(active=True)
 
 router = APIRouter(
     prefix="/role",
@@ -23,6 +27,7 @@ router = APIRouter(
 async def get_roles(
         skip: int = 0,
         limit: int = 100,
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session),
 ):
     objects, code, indexes = await crud_role.get_all_role(db=session, skip=skip, limit=limit)
@@ -34,6 +39,7 @@ async def get_roles(
             )
 async def get_role(
         role_id: int,
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session),
 ):
     obj, code, indexes = await crud_role.get_role_by_id(db=session, role_id=role_id)
