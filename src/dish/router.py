ceb95bf89a1cec88675c27bfc12a157fe29599a0
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-06-06 19:53:53 +0300

    add current_user in router: - dish - selling_point - selling_point_type

--- src/dish/router.py
+++ src/dish/router.py
@@ -3,13 +3,16 @@ import logging
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from auth.base_config import fastapi_users
+from auth.models import User
 from core.response import SingleEntityResponse, ListOfEntityResponse
 from database import get_async_session
 from dish.crud import crud_dish
 from dish.getters import getting_dish
 from dish.schemas import DishCreate, DishUpdate
 
-from promo.crud import crud_promo
+current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
+current_active_user = fastapi_users.current_user(active=True)
 
 router = APIRouter(
     prefix="/dish",
@@ -26,6 +29,7 @@ router = APIRouter(
 async def get_dishes(
         limit: int = 100,
         skip: int = 0,
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session),
 ):
     dishes, code, indexes = await crud_dish.get_all_dishes(db=session, skip=skip, limit=limit)
@@ -40,6 +44,7 @@ async def get_dishes(
 )
 async def get_dish_by_id(
         dish_id: int,
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session),
 ):
     dish, code, indexes = await crud_dish.get_dish_by_id(db=session, dish_id=dish_id)
@@ -57,6 +62,7 @@ async def get_dish_by_id(
 )
 async def create_dish(
         new_data: DishCreate,
+        user: User = Depends(current_active_superuser),
         session: AsyncSession = Depends(get_async_session),
 ):
     obj, code, indexes = await crud_dish.create_dish(db=session, new_data=new_data)
@@ -75,6 +81,7 @@ async def create_dish(
 async def update_dish(
         update_data: DishUpdate,
         dish_id: int,
+        user: User = Depends(current_active_superuser),
         session: AsyncSession = Depends(get_async_session),
 ):
     dish, code, indexes = await crud_dish.update_dish(db=session,
