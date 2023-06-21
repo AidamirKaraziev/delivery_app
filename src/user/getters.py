8086c7580ff2244418647d1e14f7cb0f04668b0b
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-06-21 14:17:29 +0300

    fix api in fastapi_users: get_me, get_id, update_me, update_id -change schemas

--- src/user/getters.py
+++ src/user/getters.py
@@ -1,25 +1,27 @@
 from typing import Optional
 from fastapi import Request
 
-from auth.schemas import UserGet
+from auth.schemas import UserGet, UserRead
 from role.getters import getting_role
 
 
-def getting_user(obj: UserGet,  request: Optional[Request]) -> Optional[UserGet]:
+def getting_user(obj: UserRead,  request: Optional[Request]) -> Optional[UserRead]:
     if request is not None:
         url = request.url.hostname + ":8000" + "/static/"
         if obj.photo is not None:
             obj.photo = url + str(obj.photo)
         else:
             obj.photo = None
-    return UserGet(
+    return UserRead(
         id=obj.id,
         name=obj.name,
         photo=obj.photo,
         email=obj.email,
         phone_number=obj.phone_number,
         registered_at=obj.registered_at,  # сделать вывод в юникс секундах
-        role_id=getting_role(obj.role) if obj.role is not None else None,  # хз почему желтое не кайф
+        role_id=getting_role(obj.role) if obj.role is not None else None,
+        # role_id=obj.role_id,
+
         is_active=obj.is_active,
         is_superuser=obj.is_superuser,
         is_verified=obj.is_verified,
--- src/user/router.py
+++ src/user/router.py
@@ -2,27 +2,27 @@ import logging
 import shutil
 import time
 import os
-from typing import Optional
+from typing import Optional, Type
 
-from fastapi import APIRouter, Depends, HTTPException, Request, status, UploadFile, File
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status,  UploadFile, File
 from fastapi.responses import FileResponse
-from fastapi_users.router.common import ErrorCode, ErrorModel
-from fastapi_users.manager import BaseUserManager
-from fastapi_users import exceptions, models
 from fastapi.staticfiles import StaticFiles
+from fastapi_users import exceptions, models
+from fastapi_users.manager import BaseUserManager, UserManagerDependency
+from fastapi_users.router.common import ErrorCode, ErrorModel
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth.base_config import fastapi_users
 from auth.manager import get_user_manager
 from auth.models import User
-from auth.schemas import UserCreate, UserGet, UserRead
+from auth.schemas import UserCreate, UserRead, UserUpdate
 from core.response import ListOfEntityResponse, SingleEntityResponse
 from database import get_async_session
 
 from user.crud import crud_user
 from user.getters import getting_user
-from sqlalchemy import exc, select, delete, update
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "upload")
@@ -61,7 +61,7 @@ async def get_users(
 
 @router.post(
     path="/create-admin/",
-    response_model=UserRead,
+    response_model=SingleEntityResponse,
     status_code=status.HTTP_201_CREATED,
     name="create_admin",
     description="Создание админа",
@@ -116,14 +116,7 @@ async def create_admin(
                 "reason": e.reason,
             },
         )
-    # create_user - это User, как вывести в getting_user() SingleEntityResponse
-    return UserRead.from_orm(created_user)
-    # return SingleEntityResponse(data=getting_user(obj=created_user))
-
-
-@router.get("/test/")
-def read_root():
-    return {"hellow": "AIDAMIR"}
+    return SingleEntityResponse(data=getting_user(obj=user, request=request))
 
 
 @router.post('file/upload/')
@@ -167,6 +160,199 @@ async def add_photo(
     if not save_path:
         return HTTPException(status_code=400, detail=f"Not have save photo")
     return SingleEntityResponse(data=getting_user(user, request=request))
+
+
+def get_users_router(
+    get_user_manager: UserManagerDependency[models.UP, models.ID],
+    user_schema: Type[UserRead],
+    user_update_schema: Type[UserUpdate],
+    # authenticator: Authenticator,
+    requires_verification: bool = False,
+) -> APIRouter:
+    router = APIRouter()
+
+    get_current_active_user = fastapi_users.current_user(
+        active=True, verified=requires_verification
+    )
+    get_current_superuser = fastapi_users.current_user(
+        active=True, verified=requires_verification, superuser=True
+    )
+
+    async def get_user_or_404(
+        id: str,
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    ) -> models.UP:
+        try:
+            parsed_id = user_manager.parse_id(id)
+            return await user_manager.get(parsed_id)
+        except (exceptions.UserNotExists, exceptions.InvalidID) as e:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) from e
+
+    @router.get(
+        "/me",
+        response_model=SingleEntityResponse,
+        name="users:current_user",
+        responses={
+            status.HTTP_401_UNAUTHORIZED: {
+                "description": "Missing token or inactive user.",
+            },
+        },
+    )
+    async def me(
+        request: Request,
+        user: models.UP = Depends(get_current_active_user),
+    ):
+        return SingleEntityResponse(data=getting_user(obj=user, request=request))
+
+    @router.patch(
+        "/me",
+        response_model=SingleEntityResponse,
+        dependencies=[Depends(get_current_active_user)],
+        name="users:patch_current_user",
+        responses={
+            status.HTTP_401_UNAUTHORIZED: {
+                "description": "Missing token or inactive user.",
+            },
+            status.HTTP_400_BAD_REQUEST: {
+                "model": ErrorModel,
+                "content": {
+                    "application/json": {
+                        "examples": {
+                            ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS: {
+                                "summary": "A user with this email already exists.",
+                                "value": {
+                                    "detail": ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS
+                                },
+                            },
+                            ErrorCode.UPDATE_USER_INVALID_PASSWORD: {
+                                "summary": "Password validation failed.",
+                                "value": {
+                                    "detail": {
+                                        "code": ErrorCode.UPDATE_USER_INVALID_PASSWORD,
+                                        "reason": "Password should be"
+                                        "at least 3 characters",
+                                    }
+                                },
+                            },
+                        }
+                    }
+                },
+            },
+        },
+    )
+    async def update_me(
+        request: Request,
+        user_update: user_update_schema,
+        user: models.UP = Depends(get_current_active_user),
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    ):
+        try:
+            user = await user_manager.update(
+                user_update, user, safe=True, request=request)
+
+            return SingleEntityResponse(data=getting_user(obj=user, request=request))
+        except exceptions.InvalidPasswordException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "code": ErrorCode.UPDATE_USER_INVALID_PASSWORD,
+                    "reason": e.reason,
+                },
+            )
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                detail=ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS,
+            )
+
+    @router.get(
+        "/{id}",
+        response_model=SingleEntityResponse,
+        dependencies=[Depends(get_current_superuser)],
+        name="users:user",
+        responses={
+            status.HTTP_401_UNAUTHORIZED: {
+                "description": "Missing token or inactive user.",
+            },
+            status.HTTP_403_FORBIDDEN: {
+                "description": "Not a superuser.",
+            },
+            status.HTTP_404_NOT_FOUND: {
+                "description": "The user does not exist.",
+            },
+        },
+    )
+    async def get_user(request: Request, user=Depends(get_user_or_404)):
+        return SingleEntityResponse(data=getting_user(obj=user, request=request))
+
+    @router.patch(
+        "/{id}",
+        response_model=SingleEntityResponse,
+        dependencies=[Depends(get_current_superuser)],
+        name="users:patch_user",
+        responses={
+            status.HTTP_401_UNAUTHORIZED: {
+                "description": "Missing token or inactive user.",
+            },
+            status.HTTP_403_FORBIDDEN: {
+                "description": "Not a superuser.",
+            },
+            status.HTTP_404_NOT_FOUND: {
+                "description": "The user does not exist.",
+            },
+            status.HTTP_400_BAD_REQUEST: {
+                "model": ErrorModel,
+                "content": {
+                    "application/json": {
+                        "examples": {
+                            ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS: {
+                                "summary": "A user with this email already exists.",
+                                "value": {
+                                    "detail": ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS
+                                },
+                            },
+                            ErrorCode.UPDATE_USER_INVALID_PASSWORD: {
+                                "summary": "Password validation failed.",
+                                "value": {
+                                    "detail": {
+                                        "code": ErrorCode.UPDATE_USER_INVALID_PASSWORD,
+                                        "reason": "Password should be"
+                                        "at least 3 characters",
+                                    }
+                                },
+                            },
+                        }
+                    }
+                },
+            },
+        },
+    )
+    async def update_user(
+        user_update: user_update_schema,
+        request: Request,
+        user=Depends(get_user_or_404),
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    ):
+        try:
+            user = await user_manager.update(
+                user_update, user, safe=False, request=request)
+
+            return SingleEntityResponse(data=getting_user(obj=user, request=request))
+        except exceptions.InvalidPasswordException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "code": ErrorCode.UPDATE_USER_INVALID_PASSWORD,
+                    "reason": e.reason,
+                },
+            )
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                detail=ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS,
+            )
+
+    return router
 
 
 if __name__ == "__main__":
