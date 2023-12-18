79c1275e4ba0c0d457499c305eb03d2ab4672d1c
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2023-12-18 03:55:18 +0300

    change alembic

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -49,7 +49,7 @@ target_metadata = [metadata_role, metadata_sp_type, metadata_auth, metadata_prom
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Run alembic in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -73,7 +73,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Run alembic in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
