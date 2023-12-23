991cad02eda32a150707077083361aa8b6a33be6
Karaji_jin2 <ajdamir.kareaziev@gmail.com>
2023-12-23 12:03:15 +0300

    change to nice auto creating base data

--- src/core/initial_data.py
+++ src/core/initial_data.py
@@ -2,102 +2,63 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
+from config import STATUS_CREATE, STATUS_IN_PROCESS, STATUS_DONE, ROLE_ADMIN, ROLE_USER
+from order_status.models import OrderStatus
 from role.models import Role
-from order_status.models import Status
 from database import get_async_session
 
 
-async def check_roles(session: AsyncSession = Depends(get_async_session)):
-    # Проверяем наличие ролей
-    query_admin = select(Role).where(Role.name == 'admin', Role.id == 1)
-    query_user = select(Role).where(Role.name == 'user', Role.id == 2)
-
-    admin_role = await session.execute(query_admin)
-    user_role = await session.execute(query_user)
-
-    if admin_role.scalar_one_or_none() is not None:
-        admin_role_is_exist = True
-    else:
-        admin_role_is_exist = False
-
-    if user_role.scalar_one_or_none() is not None:
-        user_role_is_exist = True
-    else:
-        user_role_is_exist = False
-
-    return admin_role_is_exist, user_role_is_exist
-
-
-async def check_order_statuses(session: AsyncSession = Depends(get_async_session)):
-    # проверить наличие статусов
-    query_created = select(Status).where(Status.name == 'created', Status.id == 1)
-    query_in_progress = select(Status).where(Status.name == 'in_progress', Status.id == 2)
-    query_completed = select(Status).where(Status.name == 'completed', Status.id == 3)
-
-    status_created = await session.execute(query_created)
-    status_in_progress = await session.execute(query_in_progress)
-    status_completed = await session.execute(query_completed)
-    statuses = [status_created, status_in_progress, status_completed]
-
-    result = []
-    for status in statuses:
-        if status.scalar_one_or_none() is not None:
-            result.append(True)
-        else:
-            result.append(False)
-
-    return result
+async def check_status(session: AsyncSession = Depends(get_async_session)):
+    check_list = [
+        OrderStatus(id=STATUS_CREATE.id, name=STATUS_CREATE.name),
+        OrderStatus(id=STATUS_IN_PROCESS.id, name=STATUS_IN_PROCESS.name),
+        OrderStatus(id=STATUS_DONE.id, name=STATUS_DONE.name)
+                  ]
+    creation_list = []
+    for obj in check_list:
+        query = select(OrderStatus).where(OrderStatus.name == obj.name, OrderStatus.id == obj.id)
+        obj_type = await session.execute(query)
+        if obj_type.scalar_one_or_none() is None:
+            creation_list.append(obj)
+    return creation_list
 
 
 async def create_statuses():
     async for db in get_async_session():
-        statuses = []
-
-        statuses_is_exist = await check_order_statuses(db)
-        if statuses_is_exist[0] and statuses_is_exist[1] and statuses_is_exist[2]:
-            return  # Начальные данные уже существуют
-
-        if not statuses_is_exist[0]:
-            statuses.append(Status(id=1, name='created'))
-        if not statuses_is_exist[1]:
-            statuses.append(Status(id=2, name='in_progress'))
-        if not statuses_is_exist[2]:
-            statuses.append(Status(id=3, name='completed'))
-
-        [db.add(status) for status in statuses]
+        creation_list = await check_status(db)
+        [db.add(obj) for obj in creation_list]
         await db.commit()
         await db.close()
 
 
+async def check_role(session: AsyncSession = Depends(get_async_session)):
+    check_list = [
+        Role(id=ROLE_ADMIN.id, name=ROLE_ADMIN.name),
+        Role(id=ROLE_USER.id, name=ROLE_USER.name)
+                  ]
+    creation_list = []
+    for obj in check_list:
+        query = select(Role).where(Role.name == obj.name, Role.id == obj.id)
+        obj_type = await session.execute(query)
+        if obj_type.scalar_one_or_none() is None:
+            creation_list.append(obj)
+    return creation_list
+
+
 async def create_roles():
     async for db in get_async_session():
-        roles = []
-
-        # Проверка наличия данных
-        admin_role_is_exist, user_role_is_exist = await check_roles(db)
-
-        if admin_role_is_exist and user_role_is_exist:
-            return  # Начальные данные уже существуют
-
-        # Создание недостающих ролей
-        if not admin_role_is_exist:
-            roles.append(Role(id=1, name='admin', permissions=None))
-        if not user_role_is_exist:
-            roles.append(Role(id=2, name='user', permissions=None))
-
-        [db.add(role) for role in roles]
+        creation_list = await check_status(db)
+        [db.add(obj) for obj in creation_list]
         await db.commit()
         await db.close()
 
 
 async def create_initial_data():
     try:
-        pass
         await create_roles()
     except Exception as e:
         print(e)
     try:
-        pass
         await create_statuses()
     except Exception as e:
         print(e)
