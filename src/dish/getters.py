4d94697a4da4577136a14a75494da25cf745d476
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2024-02-04 19:49:49 +0300

    fix import app_port

--- src/dish/getters.py
+++ src/dish/getters.py
@@ -3,10 +3,12 @@ from typing import Optional
 from dish.schemas import DishGet
 from promo.getters import getting_promo
 
+from config import APP_PORT
+
 
 def getting_dish(obj: DishGet, request: Request) -> Optional[DishGet]:
     if request is not None:
-        url = request.url.hostname + ":8000" + "/static/"
+        url = request.url.hostname + f":{APP_PORT}" + "/static/"
         if obj.main_photo is not None:
             obj.main_photo = url + str(obj.main_photo)
         else:
